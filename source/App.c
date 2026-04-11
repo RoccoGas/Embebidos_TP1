ad46709b6999985667f80488a527b5994f32bbcf
RoccoGas <80708747+RoccoGas@users.noreply.github.com>
2026-04-11 15:26:54 -0300

    Implement state machine for access control
    Refactored App.c to implement state machine for user access control ??

--- source/App.c
+++ source/App.c
@@ -9,21 +9,76 @@
 #include "encoder.h"
 #include "magtek.h"
 
-#define TEST_PERIOD_MS TIMER_MS2TICKS(1000)
+enum {IDLE, ENTER_ID, VERIFY_ID, ENTER_PASSWORD, VERIFY_PASSWORD,
+		ACCESS_GRANTED, ACCESS_DENIED }app_states_enum ;
+
+app_states_enum appState = IDLE;
 
 
-static int led = 0;
-static rotary_event_t event = 0;
+static char id[8];
 
-tim_id_t testTimer;
+rotary_event_t rotaryEvent = IDLE;
+
 
 void App_Init(void)
 {
-
+	timerInit();
+	displayInit();
+	encoderInit();
+	magtekInit();
 
 }
 void App_Run(void)
 {
+	timerUpdate();
+	switch(appState){
+	case IDLE:
+		if(magtekDataReady()){
+			char auxBuffer[MAGTEK_MAX_CHARS+1];
+			char lengthOfData;
+			magtek_result_t magtekResult = magtekGetData(auxbuffer, &lengthOfData);
+
+			if(magtekResult == MAGTEK_OK){
+				appState = VERIFY_ID;
+			}
+			else{
+				//DISPLAY ERROR
+			}
+
+		}
+
+		if(rotaryEvent == BUTTON_PRESS || rotaryEvent == LONG_BUTTON_PRESS ){
+			appState = ENTER_ID;
+		}
+		break;
+	case ENTER_ID:
+		bool enteringID = true;
+		char characterBeingChosen = '0';
+		bool modeConfirmInput = false;
+		while(enteringID){
+
+			switch(rotaryEvent){
+			case LEFT_TURN:
+
+				break;
+			default:
+				break;
+			}
+
+		}
+
+
+		break;
+	case VERIFY_ID:
+		break;
+	case ENTER_PASSWORD:
+		break;
+	case VERIFY_PASSWORD:
+		break;
+	case ACCESS_GRANTED:
+		break;
+	case ACCESS_DENIED:
+			break;
+	}
 
 }
-
